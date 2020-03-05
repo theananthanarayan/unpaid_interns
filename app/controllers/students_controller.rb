ea979219f547b63a1f8c393ff8db76ffe09ebf2d
EC2 Default User <ec2-user@ip-172-31-20-92.us-east-2.compute.internal>
2020-03-05 03:51:14 +0000

    Profile Page now takes info from the database instead of hard-coding

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,7 @@
 class StudentsController < ApplicationController
   def profile
+    @student = Student.find(1)
+    #Just a basic way to output the one person for now.  This will be fleshed out later
   end
   
   def message
