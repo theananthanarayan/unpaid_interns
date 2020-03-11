19f0f62435fb0fa2690fff6a40348d30cdd209a0
EC2 Default User <ec2-user@ip-172-31-20-92.us-east-2.compute.internal>
2020-03-11 19:08:14 +0000

    database recreated, and seed was added.  There are a few errors with this build.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create([{firstName: "Jack", lastName: "Black", classYear: 2010, advisor: "Terry Crews", intro: "Hey how's it going? I'm Jack! And today, I'm here to tell you about the word, octagon! Now octagon is an amazing shape that has eight fantastic sides, and eight awesome angles!", research: ["Octagons", "Stop Signs"], colleagues: ["Elmo", "Terry Crews"], careers: ["Kung Fu Panda"], profilePic: "JackElmo.png"}])
