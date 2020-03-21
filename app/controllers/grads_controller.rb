b206d399741e8f67ea4854578fdb0bcba7a872df
EC2 Default User <ec2-user@ip-172-31-20-92.us-east-2.compute.internal>
2020-03-21 22:05:32 +0000

    Login test added.  It works

--- app/controllers/grads_controller.rb
+++ app/controllers/grads_controller.rb
@@ -1,5 +1,13 @@
 class GradsController < ApplicationController
+  
   def index
+    if params[:password] == nil || params[:password] == ""
+      @introMsg = "Guest"
+    else
+      tempUser = User.where(password: params[:password])
+      @introMsg = Student.find(tempUser.pluck(:studentID)[0]).firstName
+      #@introMsg = Student.find(1).firstName
+    end
   end
   
   def new
