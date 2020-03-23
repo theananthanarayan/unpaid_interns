aa4a420991996b63cec63701fb919520a98988f7
EC2 Default User <ec2-user@ip-172-31-20-92.us-east-2.compute.internal>
2020-03-23 21:01:00 +0000

    Added login feature, fleshed out profile compatibility around the site

--- app/controllers/grads_controller.rb
+++ app/controllers/grads_controller.rb
@@ -2,20 +2,30 @@ class GradsController < ApplicationController
   
   def index
     @pw = params[:password]
-    if @pw == nil || @pw == ""
+    @em = params[:email]
+    if @pw == nil || @pw == "" || @em == nil || @em == ""
+      params[:password] = ""
+      params[:email] = ""
+      @pw = params[:password]
+      @em = params[:email]
       @introMsg = "Guest"
     else
-      tempUser = User.where(password: @pw)
+      tempUser = User.where(password: @pw, email: @em)
       @introMsg = Student.find(tempUser.pluck(:studentID)[0]).firstName + " " + Student.find(tempUser.pluck(:studentID)[0]).lastName
     end
   end
   
   def search
     @pw = params[:password]
-    if params[:password] == nil || params[:password] == ""
+    @em = params[:email]
+    if @pw == nil || @pw == "" || @em == nil || @em == ""
+      params[:password] = ""
+      params[:email] = ""
+      @pw = params[:password]
+      @em = params[:email]
       @introMsg = "Guest"
     else
-      tempUser = User.where(password: params[:password])
+      tempUser = User.where(password: @pw, email: @em)
       @introMsg = Student.find(tempUser.pluck(:studentID)[0]).firstName + " " + Student.find(tempUser.pluck(:studentID)[0]).lastName
     end
   end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,15 +1,17 @@
 class StudentsController < ApplicationController
   def profile
     @pw = params[:password]
+    @em = params[:email]
     @student = Student.find(1)
     @studName = @student.firstName + " " + @student.lastName
     
-    if(User.where(password: @pw).pluck(:studentID)[0] == @student.id)
+    if(User.where(password: @pw, email: @em).pluck(:studentID)[0] == @student.id)
       @studName = @studName + " (You)"
     end
   end
   
   def message
     @pw = params[:password]
+    @em = params[:email]
   end 
 end
