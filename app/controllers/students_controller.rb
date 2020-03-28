8be880d75493ba25cd06007ef3a58b1a825bc49d
dina-mayett <dina.mayett@gmail.com>
2020-03-28 16:38:01 +0000

    search fixed default value and search function

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,4 +14,50 @@ class StudentsController < ApplicationController
     @pw = params[:password]
     @em = params[:email]
   end 
+
+
+
+  
+  def searchBox
+    @fn = params[:first_name]
+    @ln = params[:last_name]
+    @adv = params[:advisor]
+    @clsyr = params[:class_year]
+    if @fn == nil || @fn == ""
+      @result = Student.where(lastName: @ln, advisor: @adv, classYear: @clsyr).select("field1, field2, field3, field4")
+    elsif @ln == nil|| @ln == ""
+      @result = Student.where(firstName: @fn, advisor: @adv, classYear: @clsyr).select("field1, field2, field3, field4")
+    elsif @adv == nil || @adv == ""
+      @result = Student.where(firstName: @fn, lastName: @ln, classYear: @clsyr).select("field1, field2, field3, field4")
+    elsif @clsyr == nil || @clsyr == ""
+      @result = Student.where(firstName: @fn, lastName: @ln, advisor: @adv).select("field1, field2, field3, field4")
+    elsif @fn == nil || @fn == "" && @ln == nil|| @ln == ""
+      @result = Student.where(advisor: @adv, classYear: @clsyr).select("field1, field2, field3, field4")
+    elsif @fn == nil || @fn == "" && @adv == nil || @adv == ""
+      @result = Student.where(lastName: @ln, classYear: @clsyr).select("field1, field2, field3, field4")
+    elsif @fn == nil || @fn == "" && @clsyr == nil || @clsyr == ""
+      @result = Student.where(lastName: @ln, advisor: @adv).select("field1, field2, field3, field4")
+    elsif @ln == nil|| @ln == "" && @adv == nil || @adv == ""
+      @result = Student.where(firstName: @fn, classYear: @clsyr).select("field1, field2, field3, field4")
+    elsif @ln == nil|| @ln == "" && @clsyr == nil || @clsyr == ""
+      @result = Student.where(firstName: @fn, advisor: @adv).select("field1, field2, field3, field4")
+    elsif @adv == nil || @adv == "" && @clsyr == nil || @clsyr == ""
+      @result = Student.where(firstName: @fn, lastName: @ln).select("field1, field2, field3, field4")
+    elsif @fn == nil || @fn == "" && @ln == nil|| @ln == "" && @adv == nil || @adv == ""
+      @result = Student.where(classYear: @clsyr).select("field1, field2, field3, field4")
+    elsif @fn == nil || @fn == "" && @ln == nil|| @ln == "" && @clsyr == nil || @clsyr == ""
+      @result = Student.where(advisor: @adv).select("field1, field2, field3, field4")
+    elsif @ln == nil|| @ln == "" && @adv == nil || @adv == "" && @clsyr == nil || @clsyr == ""
+      @result = Student.where(firstName: @fn).select("field1, field2, field3, field4")
+    elsif @fn == nil || @fn == "" && @adv == nil || @adv == "" && @clsyr == nil || @clsyr == ""
+      @result = Student.where(lastName: @ln).select("field1, field2, field3, field4")
+    elsif @fn == nil || @fn == "" && @ln == nil|| @ln == "" && @adv == nil || @adv == "" && @clsyr == nil || @clsyr == ""
+      flash[:notice] = "Please fill out the form"
+    else 
+      @result = Student.where(firstName: @fn, lastName: @ln, advisor: @adv, classYear: @clsyr).select("field1, field2, field3, field4")
+    end 
+  end
+  
+
+
 end
