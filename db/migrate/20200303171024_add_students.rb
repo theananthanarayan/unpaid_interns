f0adb6ed5ef484aa4f6b0c0230fd65ef72d03ada
EC2 Default User <ec2-user@ip-172-31-20-92.us-east-2.compute.internal>
2020-03-28 01:48:53 +0000

    Preparing to launch to Heroku.

--- db/migrate/20200303171024_add_students.rb
+++ db/migrate/20200303171024_add_students.rb
@@ -1,5 +1,5 @@
-class AddStudents < ActiveRecord::Migration
-  def up
+class AddStudents < ActiveRecord::Migration[5.0]
+  def change
     create_table :students do |t|
       t.string :firstName
       t.string :lastName
