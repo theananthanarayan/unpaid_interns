b323cc4e173bca87b23ba137b447a743f5586aec
dina-mayett <dina.mayett@gmail.com>
2020-04-10 03:28:47 +0000

    search, profile

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -65,11 +65,16 @@ class StudentsController < ApplicationController
   end
   
   def profile
-    @pw = session[:password]
-    @em = session[:email]
-    @student = Student.where(password: @pw, email: @em)
-    @student = @student.take()
-     
+    if params.has_key?(:id)
+      @student = Student.find(params[:id])
+    elsif session.has_key?(:password) and session.has_key?(:email)
+      @pw = session[:password]
+      @em = session[:email]
+      @student = Student.where(password: @pw, email: @em)
+      @student = @student.take()
+    else 
+      @student = Student.find(1)    #temp invalid student handling 
+    end
   end
 
   def edit
@@ -89,42 +94,16 @@ class StudentsController < ApplicationController
   end 
   
   def searchBox
-    @fn = params[:first_name]
-    @ln = params[:last_name]
-    @adv = params[:advisor]
-    @clsyr = params[:class_year]
-    if @fn == nil || @fn == ""
-      @result = Student.where(lastName: @ln, advisor: @adv, classYear: @clsyr).select("field1, field2, field3, field4")
-    elsif @ln == nil|| @ln == ""
-      @result = Student.where(firstName: @fn, advisor: @adv, classYear: @clsyr).select("field1, field2, field3, field4")
-    elsif @adv == nil || @adv == ""
-      @result = Student.where(firstName: @fn, lastName: @ln, classYear: @clsyr).select("field1, field2, field3, field4")
-    elsif @clsyr == nil || @clsyr == ""
-      @result = Student.where(firstName: @fn, lastName: @ln, advisor: @adv).select("field1, field2, field3, field4")
-    elsif @fn == nil || @fn == "" && @ln == nil|| @ln == ""
-      @result = Student.where(advisor: @adv, classYear: @clsyr).select("field1, field2, field3, field4")
-    elsif @fn == nil || @fn == "" && @adv == nil || @adv == ""
-      @result = Student.where(lastName: @ln, classYear: @clsyr).select("field1, field2, field3, field4")
-    elsif @fn == nil || @fn == "" && @clsyr == nil || @clsyr == ""
-      @result = Student.where(lastName: @ln, advisor: @adv).select("field1, field2, field3, field4")
-    elsif @ln == nil|| @ln == "" && @adv == nil || @adv == ""
-      @result = Student.where(firstName: @fn, classYear: @clsyr).select("field1, field2, field3, field4")
-    elsif @ln == nil|| @ln == "" && @clsyr == nil || @clsyr == ""
-      @result = Student.where(firstName: @fn, advisor: @adv).select("field1, field2, field3, field4")
-    elsif @adv == nil || @adv == "" && @clsyr == nil || @clsyr == ""
-      @result = Student.where(firstName: @fn, lastName: @ln).select("field1, field2, field3, field4")
-    elsif @fn == nil || @fn == "" && @ln == nil|| @ln == "" && @adv == nil || @adv == ""
-      @result = Student.where(classYear: @clsyr).select("field1, field2, field3, field4")
-    elsif @fn == nil || @fn == "" && @ln == nil|| @ln == "" && @clsyr == nil || @clsyr == ""
-      @result = Student.where(advisor: @adv).select("field1, field2, field3, field4")
-    elsif @ln == nil|| @ln == "" && @adv == nil || @adv == "" && @clsyr == nil || @clsyr == ""
-      @result = Student.where(firstName: @fn).select("field1, field2, field3, field4")
-    elsif @fn == nil || @fn == "" && @adv == nil || @adv == "" && @clsyr == nil || @clsyr == ""
-      @result = Student.where(lastName: @ln).select("field1, field2, field3, field4")
-    elsif @fn == nil || @fn == "" && @ln == nil|| @ln == "" && @adv == nil || @adv == "" && @clsyr == nil || @clsyr == ""
-      flash[:notice] = "Please fill out the form"
-    else 
-      @result = Student.where(firstName: @fn, lastName: @ln, advisor: @adv, classYear: @clsyr).select("field1, field2, field3, field4")
-    end 
+    attributes = [:firstName, :lastName, :advisor, :classYear] 
+    query = {}  
+    attributes.each do |attribute|
+      if params[attribute]!= ''
+        query[attribute]=params[attribute]
+      end
+    end
+    
+    @result = Student.where(query).select(attributes)
   end
-end
+end 
+    
+    
